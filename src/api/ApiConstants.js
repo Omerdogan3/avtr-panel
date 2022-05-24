61a36e10d455a8ab5cb0ff99aa273f6e70890acb
Omer Dogan <omerdogan3@gmail.com>
2022-05-24 14:49:50 +0300

    api url update

--- src/api/ApiConstants.js
+++ src/api/ApiConstants.js
@@ -1,7 +1,7 @@
 const ApiConstants = {
   // BASE_URL: "https://testing-dot-project-unite-361.uc.r.appspot.com/",
-  // BASE_URL: "https://project-unite-361.uc.r.appspot.com",
-  BASE_URL: "http://0.0.0.0:8002",
+  BASE_URL: "http://api.avtr.studio",
+  // BASE_URL: "http://0.0.0.0:8002",
   CHECK_PANEL_USER: "checkPanelUser",
   PANEL_USERS: "panelUsers",
   NEW_BLEND_SHAPE: "newBlendShape",
